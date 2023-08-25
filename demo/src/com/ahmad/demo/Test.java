560eb90ba00847d0316ba48233bb46607fba6e9e
ahmad-brwd <ahmadbrwdarsalan@gmail.com>
2023-08-26 01:06:23 +0530

    Method call.

--- demo/src/com/ahmad/demo/Test.java
+++ demo/src/com/ahmad/demo/Test.java
@@ -3,7 +3,9 @@ package com.ahmad.demo;
 public class Test {
 
 	public static void main(String[] args) {
-		
+		Test test = new Test();
+		int result = test.add(10, 20);
+		System.out.println("Result  : "+result);
 	}
 	public int add(int a, int b){
 		return a+b;
