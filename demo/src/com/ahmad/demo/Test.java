e9816f72e9f0c8a6cf2768d0f2ff8d73c8d9d2c7
ahmad-brwd <142157618+ahmad-brwd@users.noreply.github.com>
2023-08-30 20:35:30 +0530

    Update Test.java

--- demo/src/com/ahmad/demo/Test.java
+++ demo/src/com/ahmad/demo/Test.java
@@ -7,6 +7,7 @@ public class Test {
 		int result = test.add(10, 20);
 		System.out.println("Result  : "+result);
 	}
+	// method will return the sum of two number's
 	public int add(int a, int b){
 		System.out.println(a+" : "+b);
 		return a+b;
