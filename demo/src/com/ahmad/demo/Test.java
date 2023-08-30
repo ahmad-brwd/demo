501cbd3895be6237893f9218cd6a497de76a5265
ahmad-brwd <142157618+ahmad-brwd@users.noreply.github.com>
2023-08-30 21:01:26 +0530

    Update Test.java

--- demo/src/com/ahmad/demo/Test.java
+++ demo/src/com/ahmad/demo/Test.java
@@ -2,7 +2,7 @@ package com.ahmad.demo;
 
 public class Test {
 
-	// main method
+	// Java main method
 	public static void main(String[] args) {
 		Test test = new Test();
 		int result = test.add(10, 20);
